584b45ce24fbf610cddae104a1b8fd8feb093022
freedom469 <bamjnr99@gmail.com>
2023-06-06 12:50:48 -0700

    inserting into a linkef list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ *
+ * listint_t *insert_node - inserts a node into a list 
+ *
+ *@head: head node of the list to be inserted
+ *
+ *@number: value of the list to be inserted
+ *
+ *Return: the address of the new node, or NULL if it failed
+ *
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+	listint_t *temp;
+
+	if (!head->next)
+	{
+		
+	}
+
+
+}
