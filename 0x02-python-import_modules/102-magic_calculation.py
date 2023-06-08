26e5cb00f81fde14ef9afaff7e5e323624109c07
freedom469 <bamjnr99@gmail.com>
2023-06-08 05:38:55 -0700

    ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 
+from magic_calculation_102 import add, sub
+
+def magic_calculation(a, b):
+    if a < b:
+        c = a + b
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+
 if __name__ == "__main__":
-    from magic_calculation_102 import add, sub
+    result = magic_calculation(4, 7)
+    print(result)
 
-    def magic_calculation(a, b):
-
-        if a < b:
-
-            c = a + b
-
-            for i in range(4, 6):
-
-                c = add(c, i)
-
-            return c
-
-        else:
-            return sub(a, b)
