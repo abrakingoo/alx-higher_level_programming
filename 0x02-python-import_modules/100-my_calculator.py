d2ab05367bbcc78bac09eecdcefe5895b426a5db
freedom469 <bamjnr99@gmail.com>
2023-06-08 04:20:04 -0700

    Changed match case with the if statement

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,26 +17,18 @@ if __name__ == "__main__":
     num2 = int(argv[3])
     operator = argv[2]
 
-    match operator:
+    if operator == "+":
+        print("{:d} + {:d} = {}".format(num1, num2, add(num1, num2)))
 
-        case "+":
+    elif operator == "-":
+        print("{:d} - {:d} = {}".format(num1, num2, sub(num1, num2)))
 
-            print("{:d} + {:d} = {}".format(num1, num2, add(num1, num2)))
+    elif operator == "+":
+        print("{:d} * {:d} = {}".format(num1, num2, mul(num1, num2)))
 
-        case "-":
+    elif operator == "/":
+        print("{:d} / {:d} = {}".format(num1, num2, div(num1, num2)))
 
-            print("{:d} - {:d} = {}".format(num1, num2, sub(num1, num2)))
-
-        case "+":
-
-            print("{:d} * {:d} = {}".format(num1, num2, mul(num1, num2)))
-
-        case "/":
-
-            print("{:d} / {:d} = {}".format(num1, num2, div(num1, num2)))
-
-        case _:
-
-            print("Unknown operator. Available operators: +, -, * and /")
-
-            exit(1)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
