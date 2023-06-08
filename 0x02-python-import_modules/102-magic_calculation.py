8b4694d259c3e68bacae682facca58b255cd7b9a
freedom469 <bamjnr99@gmail.com>
2023-06-08 05:36:38 -0700

    ByteCode -> Python #3

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from magic_calculation_102 import add, sub
+
+    def magic_calculation(a, b):
+
+        if a < b:
+
+            c = a + b
+
+            for i in range(4, 6):
+
+                c = add(c, i)
+
+            return c
+
+        else:
+            return sub(a, b)
