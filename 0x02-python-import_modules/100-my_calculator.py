73329e5209e440b156eeb3693621cb9e7a0e8c7c
freedom469 <bamjnr99@gmail.com>
2023-06-08 04:43:49 -0700

    Changed match case with the if statement

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,13 +19,13 @@ if __name__ == "__main__":
     operators = ["+", "-", "*", "/"]
 
     if operator == "+":
-        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+        print("{} + {} = {}".format(num1, num2, add(num1, num2)))
 
     elif operator == "-":
-        print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
+        print("{} - {} = {}".format(num1, num2, sub(num1, num2)))
 
     elif operator == "*":
-        print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
+        print("{} * {} = {}".format(num1, num2, mul(num1, num2)))
 
     elif operator == "/":
         print("{} / {} = {}".format(num1, num2, div(num1, num2)))
