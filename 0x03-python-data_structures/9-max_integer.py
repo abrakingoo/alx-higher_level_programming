1d1cadf6ac195a1409558484e0c4b4e1aa5daeeb
freedom469 <bamjnr99@gmail.com>
2023-06-10 06:03:34 -0700

    fixed pycodestyle

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,11 @@
 
 def max_integer(my_list=[]):
 
-    if my_list:
+    if not my_list:
+
+        return None
+
+    else:
 
         max_value = 0
 
@@ -13,6 +17,3 @@ def max_integer(my_list=[]):
                 max_value = my_list[i]
 
         return max_value
-
-    else:
-        return None
