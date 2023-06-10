36034cd12ffcdb5ee385f4d38329f44d80b2d881
freedom469 <bamjnr99@gmail.com>
2023-06-10 05:49:00 -0700

    added a condition to check if the passed list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    for i in range(length - 1, - 1, - 1):
-        print("{:d}".format(my_list[i]))
+
+    if my_list:
+        length = len(my_list)
+        for i in range(length - 1, - 1, - 1):
+            print("{:d}".format(my_list[i]))
