60ddce50839b429355d630fba0a9b56271456255
freedom469 <bamjnr99@gmail.com>
2023-06-10 05:44:59 -0700

    Fixed theincorrect output error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j == len(matrix[i]) - 1:
+                    print("{:d}".format(matrix[i][j]), end="")
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
 
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-
-            if column != len(matrix[row]) - 1:
-
-                print("{:d}".format(matrix[row][column]), end=' ')
-
-            else:
-                print("{:d}".format(matrix[row][column]))
+            print()
