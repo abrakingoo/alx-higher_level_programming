08220af00ae176695411a108ebe76ca6473861e6
freedom469 <bamjnr99@gmail.com>
2023-06-10 04:59:52 -0700

    fixed the ouput from sting casting integers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     length = len(my_list)
-    rev_list = my_list[::-1]
-
-    for i in range(length):
-        print("{:d}".format(rev_list[i]))
+    for i in range(length - 1, - 1, - 1):
+        print("{:d}".format(my_list[i]))
