8dd8292f12005883918286e19fe72fa847739d89
freedom469 <bamjnr99@gmail.com>
2023-06-10 21:01:05 -0700

    a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,42 +5,37 @@
  * @head: pointer to pointer of first node of listint_t list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
+
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
+	if (*head == NULL)
 		return (1);
 
-	listint_t *slow = *head;
-	listint_t *fast = *head;
+	listint_t *slow, *fast, *prev, *next;
+	slow = fast = prev = next = NULL;
+	slow = fast = *head;
 
-	while (fast->next != NULL && fast->next->next != NULL)
+	while (fast != NULL && fast->next != NULL)
 	{
 		fast = fast->next->next;
+		next = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = next;
+	}
+
+	if (fast != NULL)
+		slow = slow->next;
+
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);
+
+		prev = prev->next;
 		slow = slow->next;
 	}
 
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
-	listint_t *current = slow->next;
-
-	while (current != NULL)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
-
-	listint_t *firstHalf = *head;
-	listint_t *secondHalf = prev;
-
-	while (secondHalf != NULL)
-	{
-		if (firstHalf->n != secondHalf->n)
-			return (0);
-
-		firstHalf = firstHalf->next;
-		secondHalf = secondHalf->next;
-	}
 	return (1);
 }
+
