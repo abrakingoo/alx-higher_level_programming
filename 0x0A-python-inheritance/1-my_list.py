b9360d6b21be4df586f84caf783c3ca00382cca4
freedom469 <bamjnr99@gmail.com>
2023-07-10 15:32:31 +0300

    Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/user/bin/python3
+"""
+a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """
+    a class MyList that inherits from list
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in sorted (ascending) order.
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
