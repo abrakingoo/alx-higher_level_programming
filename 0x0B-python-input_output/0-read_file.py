48947f17d126f70148b262b453c8df5f9ae0c9ab
freedom469 <bamjnr99@gmail.com>
2023-07-11 17:40:21 +0300

    a function that reads a text file (UTF8) and prints it to stdout:

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+
+"""
+a function that reads a text file (UTF8) and prints it to stdout:
+"""
+
+
 def read_file(filename=""):
     """
     a function that reads a text file (UTF8) and prints it to stdout:
