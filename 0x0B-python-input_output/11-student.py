47e834f864bc36445b7224391a4f0073cd0b5893
freedom469 <bamjnr99@gmail.com>
2023-07-11 21:16:10 +0300

    Public method def reload_from_json(self, json): that replaces all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+a class Student that defines a student by
+"""
+
+
+class Student:
+    """
+    a class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Public instance attributes:
+            first_name
+            last_name
+            age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+         retrieves a dictionary representation of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            found = {}
+
+            for attr in attrs:
+                if hasattr(self, attr):
+                    found[attr] = getattr(self, attr)
+
+            return found
+
+    def reload_from_json(self, json):
+        """
+        Public method that replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
