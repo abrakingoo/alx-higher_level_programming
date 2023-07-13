a48b57b475fa24809c17826f0563f9046aff530f
freedom469 <bamjnr99@gmail.com>
2023-07-13 16:48:18 +0300

    adding the public method def to_dictionary(self): that returns the dictionary representation of a Square:

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -62,3 +62,14 @@ class Square(Rectangle):
                 self.x = args[2]
             if len(args) > 3:
                 self.y = args[3]
+
+    def to_dictionary(self):
+        """
+        returns the dictionary representation of a Square:
+        """
+        return {
+                "id": self.id,
+                "size": self.width,
+                "x": self.x,
+                "y": self.y
+                }
