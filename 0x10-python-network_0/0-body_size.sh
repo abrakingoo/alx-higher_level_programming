eef570962a3d23019dc51a58c13a8684bcf1c396
Abu <bamjnr99@gmail.com>
2023-09-28 09:00:51 -0700

    a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -Is "$1" | grep Content-Length | awk '{print $2}'
