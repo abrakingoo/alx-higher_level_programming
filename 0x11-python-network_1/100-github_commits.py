8cd88395d7936696354905e0eee3caa5e9acb2e1
abu <bamjnr99@gmail.com>
2023-09-30 01:51:49 -0700

    list 10 commits (from the most recent to oldest) of the repository

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,31 +8,18 @@ a Python script that takes 2 arguments and
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./100-github_commits.py <repository_name> <owner_name>")
-        sys.exit(1)
 
     repository_name = sys.argv[1]
     owner_name = sys.argv[2]
 
-    # Construct the GitHub API URL with line continuation
     url = (
         f"https://api.github.com/repos/{owner_name}/"
         f"{repository_name}/commits"
     )
+    response = requests.get(url)
+    commits = response.json()
 
-    try:
-        response = requests.get(url)
-        commits = response.json()
-
-        for commit in commits[:10]:  # Display the first 10 commits
-            sha = commit.get("sha")
-            author = commit.get("commit").get("author").get("name")
-            print(f"{sha}: {author}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Request error: {e}")
-        sys.exit(1)
-    except ValueError as e:
-        print(f"JSON decoding error: {e}")
-        sys.exit(1)
+    for commit in commits[:10]:
+        sha = commit.get("sha")
+        author = commit.get("commit").get("author").get("name")
+        print(f"{sha}: {author}")
