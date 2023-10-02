193b747ba98cfa5859b8710f4d2987442e4d7646
freedom469 <bamjnr99@gmail.com>
2023-10-03 01:01:18 +0300

    10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-import requests
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
-"""
-a Python script that takes 2 arguments and
- list 10 commits (from the most recent to oldest) of the repository
-"""
+import requests
 
 
 if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    repository_name = sys.argv[1]
-    owner_name = sys.argv[2]
-
-    url = (
-        f"https://api.github.com/repos/{owner_name}/"
-        f"{repository_name}/commits"
-    )
-    response = requests.get(url)
-    commits = response.json()
-
-    for commit in commits[:10]:
-        sha = commit.get("sha")
-        author = commit.get("commit").get("author").get("name")
-        print(f"{sha}: {author}")
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
